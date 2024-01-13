c7602d2a77c56e163edcc4be087a9517a94f283f
GriffithHatch <toxicbust213@gmail.com>
2024-01-13 16:30:38 -0700

    That took too long to fix. I forgot to capitalize Line in ReadLine and WriteLine

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?\n");
+        string firstname = Console.ReadLine();
+        Console.WriteLine("What is your last name?\n");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
+
     }
 }
