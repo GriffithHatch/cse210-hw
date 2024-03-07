c665192f86c88b990f432537feb886af6d253395
GriffithHatch <toxicbust213@gmail.com>
2024-03-06 22:32:17 -0700

    I really don't know what to put here this time. Guh

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new("Blue", 15.15);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new("Yellow", 15,1.5);
+        shapes.Add(shape2);
+
+        Circle shape3 = new("Purple", 3.5);
+        shapes.Add(shape3);
+
+
+        foreach(Shape s in shapes){
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The color of the shape is {color} and the area of the shape is {area}"); 
+        }
     }
 }
